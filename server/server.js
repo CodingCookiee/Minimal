6839ca4a57e7d4265353940eababb18e20228597
CodingCookiee <rzqutubawan@gmail.com>
2025-01-07 11:54:13 +0500

    Update

--- server/server.js
+++ server/server.js
@@ -1,60 +1,48 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import errorHandler from "./middleware/errorHandler.js";
-import { connectToDatabase } from "./config/database.js";
-import routes from "./routes/index.js";
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import { connectToDatabase } from './config/database.js'
+import  errorHandler  from './middleware/errorHandler.middleware.js'
+import routes from './routes/index.js'
+import cors from 'cors'
 
-dotenv.config();
+dotenv.config()
+const port = parseInt(process.env.PORT || 3000)
 
-const port = parseInt(process.env.PORT || "3000", 10);
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(errorHandler)
+app.use(cors({
+    origin: `process.env.CLIENT_URL`,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    crossOriginOpenerPolicy: "same-origin-allow-popups"
-  }),
-);
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    crossOriginOpenerPolicy: true,
+
+}))
 
 
-// Routes
-app.use("/api/auth", routes.authRoutes);
-app.use('/api/user', routes.userRoutes);
-app.use('/api/product', routes.productRoutes);
-app.use('/api/coupon', routes.couponRoutes);
-app.use('/api/cart', routes.cartRoutes);
-app.use('/api/payment', routes.paymentRoutes);
-app.use('/api/analytics', routes.analyticsRoutes);
+app.use('/auth', routes.authRoutes)
+app.use('/user', routes.userRoutes)
+app.use('/product', routes.productRoutes)
+app.use('/coupon', routes.couponRoutes)
+app.use('/cart', routes.cartRoutes)
+app.use('/payment', routes.paymentRoutes)
+app.use('/analytics', routes.analyticsRoutes)
+app.get('/', (req, res) =>{
+ res.send('Server is running : Use /api to run tests')
+})
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "healthy",
-    timestamp: new Date().toISOString(),
-  });
-});
-app.get("/", (req, res) => {
-  res.send("The Server is running : Use /api to Run Tests");
-});
 
-app.use(errorHandler);
 
-app
-  .listen(port, async () => {
-    await connectToDatabase();
-    console.log(`Server running on http://localhost:${port}`);
-    console.log(`Backend allowed client: ${process.env.CLIENT_URL}`);
-  })
-  .on("error", (err) => {
-    if (err.code === "EACCES") {
-      console.log(`Port ${port} requires elevated privileges.`);
+app.listen(process.env.PORT, async () => {
+    await connectToDatabase()
+    console.log(`Server is running on port ${process.env.PORT}`)
+    console.log(`Client allowed: ${process.env.CLIENT_URL}`)
+}).on('error', (err) => {
+    if(error.code === 'EADDRINUSE') {
+        console.log('Port is already in use')
     } else {
-      console.error("Server error:", err);
+        console.log('Server Error:' ,err)
     }
-  });
+})
