e1155b6b776ffc7355443e65b234d3adbb7fdb51
CodingCookiee <rzqutubawan@gmail.com>
2025-01-15 14:19:31 +0500

    Update

---
+++ client/public/women.js
@@ -0,0 +1,370 @@
+// Importing all the files from Women/Jeans
+import BaggyBoyFriendJeans1 from "./Women/Jeans/Baggy_Boy_Friend_Jeans/10002.jpg";
+import BaggyBoyFriendJeans2 from "./Women/Jeans/Baggy_Boy_Friend_Jeans/10003.jpg";
+import BaggyBoyFriendJeans3 from "./Women/Jeans/Baggy_Boy_Friend_Jeans/10004.jpg";
+import BaggyBoyFriendJeans4 from "./Women/Jeans/Baggy_Boy_Friend_Jeans/prod.jpg";
+
+import BasicMomFitJeans1 from "./Women/Jeans/Basic_Mom_Fit_Jeans/10002.jpg";
+import BasicMomFitJeans2 from "./Women/Jeans/Basic_Mom_Fit_Jeans/10003.jpg";
+import BasicMomFitJeans3 from "./Women/Jeans/Basic_Mom_Fit_Jeans/prod.jpg";
+
+import EmbroideredStraightFitJeans1 from "./Women/Jeans/Embroidered_Straight_Fit_Jeans/10003.jpg";
+import EmbroideredStraightFitJeans2 from "./Women/Jeans/Embroidered_Straight_Fit_Jeans/prod.jpg";
+
+import HighRiseSkinnyJeans1 from "./Women/Jeans/High_Rise_Skinny_Jeans/10003.jpg";
+import HighRiseSkinnyJeans2 from "./Women/Jeans/High_Rise_Skinny_Jeans/10004.jpg";
+import HighRiseSkinnyJeans3 from "./Women/Jeans/High_Rise_Skinny_Jeans/prod.jpg";
+
+import PleatedCarrotFitTrousers1 from "./Women/Jeans/Pleated_Carrot_Fit_Trousers/10001.jpg";
+
+import SlimHighJeans1 from "./Women/Jeans/Slim_High_Jeans/172979575981c9ec171c9201ddc3399fa37f.jpg";
+import SlimHighJeans2 from "./Women/Jeans/Slim_High_Jeans/edf9105918cb708e618df2c12ed534cf1ad9.jpg";
+import SlimHighJeans3 from "./Women/Jeans/Slim_High_Jeans/hmgoepprod.jpeg";
+
+import SlimStraightHighJeans1 from "./Women/Jeans/Slim_Straight_High_Jeans/1b3dc4d9a03882bacb95eae2c068562 (1).jpg";
+import SlimStraightHighJeans2 from "./Women/Jeans/Slim_Straight_High_Jeans/hmgoepprod.jpeg";
+
+import StraightJeans1 from "./Women/Jeans/Straight_jeans/24afa01336ead670fd740f44aad1e98e86d.jpg";
+import StraightJeans2 from "./Women/Jeans/Straight_jeans/df9174a1900d9de317536029c8e269ad5e7.jpg";
+import StraightJeans3 from "./Women/Jeans/Straight_jeans/hmgoepprod.jpeg";
+
+import WideLegTrousersWithSidePipping1 from "./Women/Jeans/Wide_Leg_Trousers_With_Side_Pipping/10002.jpg";
+import WideLegTrousersWithSidePipping2 from "./Women/Jeans/Wide_Leg_Trousers_With_Side_Pipping/prod.jpg";
+
+import WideBeltedJeansLightDenimBlue1 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_blue/2fec0da7331a2461ecc1ca819a963758ba5.jpg";
+import WideBeltedJeansLightDenimBlue2 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_blue/3b285e49544c5069e65855f0af8575ab36e.jpg";
+import WideBeltedJeansLightDenimBlue3 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_blue/f3e24cf3c357be9796ea7383e3b90269c69.jpg";
+import WideBeltedJeansLightDenimBlue4 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_blue/prod.jpg";
+
+import WideBeltedJeansLightDenimWhite1 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_white/2e924bd975f5216e1ed649dbeebb11754879a87c.jpg";
+import WideBeltedJeansLightDenimWhite2 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_white/59490690be2864312b02ef1f8264ae76c6fb230c.jpg";
+import WideBeltedJeansLightDenimWhite3 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_white/b19c60c0cb5b47f86424b479b1bccef71184ea92.jpg";
+import WideBeltedJeansLightDenimWhite4 from "./Women/Jeans/Wide_belted_jeans_-_Light_denim_white/d2517068dd0c44ed391e63a7da2e0b7c16dbd5bd.jpg";
+
+import WideJeans1 from "./Women/Jeans/Wide_jeans/3744758c482ac6971109ed4e974f053c501b.jpg";
+import WideJeans2 from "./Women/Jeans/Wide_jeans/67554d5137ac61a9db84f6479df919e7e02e.jpg";
+import WideJeans3 from "./Women/Jeans/Wide_jeans/f06ec967014ffb530687e3fdc45925e4b74e.jpg";
+import WideJeans4 from "./Women/Jeans/Wide_jeans/hmgoepprod.jpeg";
+
+import BarrelHighBlack1 from "./Women/Jeans/barrel_high_black/4df5fb5448852aaea0ec7eebe086e89d816.jpg";
+import BarrelHighBlack2 from "./Women/Jeans/barrel_high_black/6db4b8d20d1386a78ea79835056582706df.jpg";
+import BarrelHighBlack3 from "./Women/Jeans/barrel_high_black/dd684644b21d853b3347d7c0c5d29f48d30.jpg";
+import BarrelHighBlack4 from "./Women/Jeans/barrel_high_black/prod.jpg";
+
+import BarrelHighBlue1 from "./Women/Jeans/barrel_high_blue/4f114a6f1bc39070cf4863d641e2858216b.jpg";
+import BarrelHighBlue2 from "./Women/Jeans/barrel_high_blue/d701b6aabdf3e3bdaca7a68190d573af9fe.jpg";
+import BarrelHighBlue3 from "./Women/Jeans/barrel_high_blue/prod.jpg";
+
+import BarrelHighDarkBlue1 from "./Women/Jeans/barrel_high_dark_blue/9b7da165a8ac5ab1315f83f74fc5cc973b0.jpg";
+import BarrelHighDarkBlue2 from "./Women/Jeans/barrel_high_dark_blue/9bc70ed674cdd965a090f955cbcb5082ac3.jpg";
+
+// Importing all the files from Women/Shirts&Blouses
+import BandCollarBlouseNavyBlue1 from "./Women/Shirts&Blouses/Band-collar_blouse_-_Navy_blue/3bac2a34e55728c9c6f453db0f187a73e92.jpg";
+import BandCollarBlouseNavyBlue2 from "./Women/Shirts&Blouses/Band-collar_blouse_-_Navy_blue/44100f6033bd5df2ed0b9ba83dea3928d33.jpg";
+import BandCollarBlouseNavyBlue3 from "./Women/Shirts&Blouses/Band-collar_blouse_-_Navy_blue/hmgoepprod.jpeg";
+
+import BasicShacket1 from "./Women/Shirts&Blouses/Basic_Shacket/10002.jpg";
+import BasicShacket2 from "./Women/Shirts&Blouses/Basic_Shacket/10003.jpg";
+import BasicShacket3 from "./Women/Shirts&Blouses/Basic_Shacket/prod.jpg";
+
+import FauxLeatherEmbroideredShirt1 from "./Women/Shirts&Blouses/Faux_Leather_Embroidered_Shirt/10002.jpg";
+import FauxLeatherEmbroideredShirt2 from "./Women/Shirts&Blouses/Faux_Leather_Embroidered_Shirt/10003.jpg";
+import FauxLeatherEmbroideredShirt3 from "./Women/Shirts&Blouses/Faux_Leather_Embroidered_Shirt/prod.jpg";
+
+import FlounceTrimmedBlouse1 from "./Women/Shirts&Blouses/Flounce-trimmed_blouse/bcba25b5c82573ee26aea76e12b37ecbaa02afdd.jpg";
+import FlounceTrimmedBlouse2 from "./Women/Shirts&Blouses/Flounce-trimmed_blouse/d26c5463bbec7e395e3c2eb633cbf0c9bed821c9.jpg";
+import FlounceTrimmedBlouse3 from "./Women/Shirts&Blouses/Flounce-trimmed_blouse/prod.jpg";
+
+import FlounceTrimmedBlouseCreamFloral1 from "./Women/Shirts&Blouses/Flounce-trimmed_blouse_-_Cream_Floral/b97849afc890def568c16993a2a128798dd.jpg";
+import FlounceTrimmedBlouseCreamFloral2 from "./Women/Shirts&Blouses/Flounce-trimmed_blouse_-_Cream_Floral/d4ec4b0f69c31dccbb793c75bea842cf717.jpg";
+import FlounceTrimmedBlouseCreamFloral3 from "./Women/Shirts&Blouses/Flounce-trimmed_blouse_-_Cream_Floral/hmgoepprod.jpeg";
+
+import LaceUpDenimShirtDenimBlue1 from "./Women/Shirts&Blouses/Lace-up_denim_shirt_-_Denim_blue/43077728be61b2b6210feaf6446b5ea2dfd.jpg";
+import LaceUpDenimShirtDenimBlue2 from "./Women/Shirts&Blouses/Lace-up_denim_shirt_-_Denim_blue/8298b5ef770e56f542e71c4fed752547961.jpg";
+import LaceUpDenimShirtDenimBlue3 from "./Women/Shirts&Blouses/Lace-up_denim_shirt_-_Denim_blue/hmgoepprod.jpeg";
+
+import LinenShirtBlack1 from "./Women/Shirts&Blouses/Linen_shirt_-_Black/670795bd23fa4867ff88ab7654dbb6cb7fe359df.jpg";
+import LinenShirtBlack2 from "./Women/Shirts&Blouses/Linen_shirt_-_Black/8ff93c7e98602c60bcc2d375be03ee8a3b5474ed.jpg";
+import LinenShirtBlack3 from "./Women/Shirts&Blouses/Linen_shirt_-_Black/hmgoepprod.jpeg";
+
+import LooseFitCottonShirt1 from "./Women/Shirts&Blouses/Loose-fit_cotton_shirt/5334569f2fd235320fd553db1c57988fb91.jpg";
+import LooseFitCottonShirt2 from "./Women/Shirts&Blouses/Loose-fit_cotton_shirt/97edbf68c4f4036d9d7aacb2e5db90eb8eb.jpg";
+import LooseFitCottonShirt3 from "./Women/Shirts&Blouses/Loose-fit_cotton_shirt/hmgoepprod.jpeg";
+
+import LyocellDenimShirt1 from "./Women/Shirts&Blouses/Lyocell_denim_shirt/47e7ff3e14875dcd2dac5405aaac2c1dd3c5e41c.jpg";
+import LyocellDenimShirt2 from "./Women/Shirts&Blouses/Lyocell_denim_shirt/7a5430943889e77e427a2e43443d9ec10baa900e.jpg";
+import LyocellDenimShirt3 from "./Women/Shirts&Blouses/Lyocell_denim_shirt/a7c61c570f9e430f8045dec737c51591716c7f4e.jpg";
+import LyocellDenimShirt4 from "./Women/Shirts&Blouses/Lyocell_denim_shirt/prod.jpg";
+
+import LyocellDenimShirtDenimBlue1 from "./Women/Shirts&Blouses/Lyocell_denim_shirt_-_Denim_blue/5d544ff0be8aca785d30b40f7953cd2f1b3.jpg";
+import LyocellDenimShirtDenimBlue2 from "./Women/Shirts&Blouses/Lyocell_denim_shirt_-_Denim_blue/de4937ac4a28ab7c9386d07857673e16eb1.jpg";
+import LyocellDenimShirtDenimBlue3 from "./Women/Shirts&Blouses/Lyocell_denim_shirt_-_Denim_blue/hmgoepprod.jpeg";
+
+import LyocellDenimShirtDenimGrey1 from "./Women/Shirts&Blouses/Lyocell_denim_shirt_-_Denim_grey/acc42c56c7b2537a7133cd4a4ace9f3a864.jpg";
+import LyocellDenimShirtDenimGrey2 from "./Women/Shirts&Blouses/Lyocell_denim_shirt_-_Denim_grey/d39771017b97ada4db3410bb2e2b29fa476.jpg";
+import LyocellDenimShirtDenimGrey3 from "./Women/Shirts&Blouses/Lyocell_denim_shirt_-_Denim_grey/hmgoepprod.jpeg";
+
+import NonDenimShacket1 from "./Women/Shirts&Blouses/Non_Denim_Shacket/10001.jpg";
+import NonDenimShacket2 from "./Women/Shirts&Blouses/Non_Denim_Shacket/10002.jpg";
+import NonDenimShacket3 from "./Women/Shirts&Blouses/Non_Denim_Shacket/prod.jpg";
+
+import OxfordShirtLightPink1 from "./Women/Shirts&Blouses/Oxford_shirt_-_Light_pink/017c59c76bf0790ba5bf34f54f48981fc4e.jpg";
+import OxfordShirtLightPink2 from "./Women/Shirts&Blouses/Oxford_shirt_-_Light_pink/349edbc0267ee0dae6071b42d1776957d9d.jpg";
+import OxfordShirtLightPink3 from "./Women/Shirts&Blouses/Oxford_shirt_-_Light_pink/hmgoepprod.jpeg";
+
+import PopoverDenimShirtCream1 from "./Women/Shirts&Blouses/Popover_denim_shirt_-_Cream/6e29b38b997e6d2a4ada235bd2a09969488.jpg";
+import PopoverDenimShirtCream2 from "./Women/Shirts&Blouses/Popover_denim_shirt_-_Cream/ba9fdcbae967fa22cf1120dc7c0047f5da5.jpg";
+import PopoverDenimShirtCream3 from "./Women/Shirts&Blouses/Popover_denim_shirt_-_Cream/hmgoepprod.jpeg";
+
+import SequinedBlouseDarkGrey1 from "./Women/Shirts&Blouses/Sequined_blouse_-_Dark_grey/9d932f43439c7683ea41416225e67b6f82f.jpg";
+import SequinedBlouseDarkGrey2 from "./Women/Shirts&Blouses/Sequined_blouse_-_Dark_grey/daa3e92deaf35e8253da4c23fc7af72f0dc.jpg";
+import SequinedBlouseDarkGrey3 from "./Women/Shirts&Blouses/Sequined_blouse_-_Dark_grey/hmgoepprod.jpeg";
+
+import WomensBlousesShirtsDenimSatinLinen1 from "./Women/Shirts&Blouses/Women_s_Blouses_Shirts_Denim,_Satin_Linen/3857701671e7b0639d48802f66de08009ee.jpg";
+import WomensBlousesShirtsDenimSatinLinen2 from "./Women/Shirts&Blouses/Women_s_Blouses_Shirts_Denim,_Satin_Linen/6d8b144666546cffcaa561b0183c9f22511.jpg";
+import WomensBlousesShirtsDenimSatinLinen3 from "./Women/Shirts&Blouses/Women_s_Blouses_Shirts_Denim,_Satin_Linen/c32c7eb9aa0c22076bf24e71a83b3fc407c.jpg";
+import WomensBlousesShirtsDenimSatinLinen4 from "./Women/Shirts&Blouses/Women_s_Blouses_Shirts_Denim,_Satin_Linen/pro.jpg";
+
+// Importing all the files from Women/Trousers
+import CigaretteTrousersBlack1 from "./Women/Trousers/Cigarette_trousers_-_Black_/089ddf722298663f24f7ce98798139b029b.jpg";
+import CigaretteTrousersBlack2 from "./Women/Trousers/Cigarette_trousers_-_Black_/20c1de12d91f5b350aacc7886f020b49b59.jpg";
+import CigaretteTrousersBlack3 from "./Women/Trousers/Cigarette_trousers_-_Black_/hmgoepprod.jpeg";
+
+import FineKnitTrousersDarkBeige1 from "./Women/Trousers/Fine-knit_trousers_-_Dark_beige/174fde7034ca51adbb5a47f7735dc3aadc1.jpg";
+import FineKnitTrousersDarkBeige2 from "./Women/Trousers/Fine-knit_trousers_-_Dark_beige/e932aa1dbb234fdec7641b36dff8deeb4d5.jpg";
+import FineKnitTrousersDarkBeige3 from "./Women/Trousers/Fine-knit_trousers_-_Dark_beige/hmgoepprod.jpeg";
+
+import FlaredRibKnitTrousersBlack1 from "./Women/Trousers/Flared_rib-knit_trousers_-_Black_-/120621674e9913343e994b497a12c7e5b73.jpg";
+import FlaredRibKnitTrousersBlack2 from "./Women/Trousers/Flared_rib-knit_trousers_-_Black_-/95aad1f3e7dd851703c1fa9640e14b53b7b.jpg";
+import FlaredRibKnitTrousersBlack3 from "./Women/Trousers/Flared_rib-knit_trousers_-_Black_-/hmgoepprod.jpeg";
+
+import FlaredSportsLeggingsInDryMove1 from "./Women/Trousers/Flared_sports_leggings_in_DryMove/3ad1bae18d45109b2b19159677314bec11b.jpg";
+import FlaredSportsLeggingsInDryMove2 from "./Women/Trousers/Flared_sports_leggings_in_DryMove/c1181a297bad814f5103944f8ee79592ecd.jpg";
+import FlaredSportsLeggingsInDryMove3 from "./Women/Trousers/Flared_sports_leggings_in_DryMove/hmgoepprod.jpeg";
+
+import HighWaistedLeggingsBlack1 from "./Women/Trousers/High-waisted_leggings_-_Black/20c1de12d91f5b350aacc7886f020b49b59.jpg";
+import HighWaistedLeggingsBlack2 from "./Women/Trousers/High-waisted_leggings_-_Black/37c128dd5a99b0bb0f1b636fed0bc49237a.jpg";
+import HighWaistedLeggingsBlack3 from "./Women/Trousers/High-waisted_leggings_-_Black/hmgoepprod.jpeg";
+
+import SweatpantsBlack1 from "./Women/Trousers/Sweatpants_-_Black/0bdcfb188f1f166d4143400a210c148f4971458c.jpg";
+import SweatpantsBlack2 from "./Women/Trousers/Sweatpants_-_Black/27d9ab967af9781bd60ce9b2a35506b6940fd930.jpg";
+import SweatpantsBlack3 from "./Women/Trousers/Sweatpants_-_Black/hmgoepprod.jpeg";
+
+import TailoredJerseyTrousersNavy1 from "./Women/Trousers/Tailored_jersey_trousers_-_Navy/2ab19c863b57bd4bc9c6051026e2656f22d.jpg";
+import TailoredJerseyTrousersNavy2 from "./Women/Trousers/Tailored_jersey_trousers_-_Navy/c6a0aa7f93e821e8f1ca9ec16ed1ebdb04c.jpg";
+import TailoredJerseyTrousersNavy3 from "./Women/Trousers/Tailored_jersey_trousers_-_Navy/e672b8361863034669e003bbb83c75d4e39.jpg";
+import TailoredJerseyTrousersNavy4 from "./Women/Trousers/Tailored_jersey_trousers_-_Navy/hmgoepprod.jpeg";
+
+import WideJoggersPigeonBlue1 from "./Women/Trousers/Wide_joggers_-_Pigeon_blue/66ae3197059836613da27824298cef90d10.jpg";
+import WideJoggersPigeonBlue2 from "./Women/Trousers/Wide_joggers_-_Pigeon_blue/a303bf247ec5abc9e41b385b2c9f66d6d10.jpg";
+import WideJoggersPigeonBlue3 from "./Women/Trousers/Wide_joggers_-_Pigeon_blue/hmgoepprod.jpeg";
+
+import WideTailoredTrousers1 from "./Women/Trousers/Wide_tailored_trousers_/807608d3c55aa13007aa31ed7d5fa391afd.jpg";
+import WideTailoredTrousers2 from "./Women/Trousers/Wide_tailored_trousers_/f0b05c6c83552b0d38ef6f0d58daa94302e.jpg";
+import WideTailoredTrousers3 from "./Women/Trousers/Wide_tailored_trousers_/hmgoepprod.jpeg";
+
+import WideTwillTrousersBlack1 from "./Women/Trousers/Wide_twill_trousers_-_Black/3270e29ab8dbcee1c9171b5560bd3f03ded.jpg";
+import WideTwillTrousersBlack2 from "./Women/Trousers/Wide_twill_trousers_-_Black/f23ff635b17ebb67292070c36e40cc8bfbb.jpg";
+import WideTwillTrousersBlack3 from "./Women/Trousers/Wide_twill_trousers_-_Black/hmgoepprod.jpeg";
+
+// Exporting all the imports as women
+export const women = {
+  jeans: {
+    
+    BaggyBoyFriendJeans: [
+      BaggyBoyFriendJeans1,
+      BaggyBoyFriendJeans2,
+      BaggyBoyFriendJeans3,
+      BaggyBoyFriendJeans4,
+    ],
+    BasicMomFitJeans: [
+      BasicMomFitJeans1,
+      BasicMomFitJeans2,
+      BasicMomFitJeans3,
+    ],
+    EmbroideredStraightFitJeans: [
+      EmbroideredStraightFitJeans1,
+      EmbroideredStraightFitJeans2,
+    ],
+    HighRiseSkinnyJeans: [
+      HighRiseSkinnyJeans1,
+      HighRiseSkinnyJeans2,
+      HighRiseSkinnyJeans3,
+    ],
+    PleatedCarrotFitTrousers: [
+      PleatedCarrotFitTrousers1,
+    ],
+    SlimHighJeans: [
+      SlimHighJeans1,
+      SlimHighJeans2,
+      SlimHighJeans3,
+    ],
+    SlimStraightHighJeans: [
+      SlimStraightHighJeans1,
+      SlimStraightHighJeans2,
+    ],
+    StraightJeans: [
+      StraightJeans1,
+      StraightJeans2,
+      StraightJeans3,
+    ],
+    WideLegTrousersWithSidePipping: [
+      WideLegTrousersWithSidePipping1,
+      WideLegTrousersWithSidePipping2,
+    ],
+    WideBeltedJeansLightDenimBlue: [
+      WideBeltedJeansLightDenimBlue1,
+      WideBeltedJeansLightDenimBlue2,
+      WideBeltedJeansLightDenimBlue3,
+      WideBeltedJeansLightDenimBlue4,
+    ],
+    WideBeltedJeansLightDenimWhite: [
+      WideBeltedJeansLightDenimWhite1,
+      WideBeltedJeansLightDenimWhite2,
+      WideBeltedJeansLightDenimWhite3,
+      WideBeltedJeansLightDenimWhite4,
+    ],
+    WideJeans: [
+      WideJeans1,
+      WideJeans2,
+      WideJeans3,
+      WideJeans4,
+    ],
+    BarrelHighBlack: [
+      BarrelHighBlack1,
+      BarrelHighBlack2,
+      BarrelHighBlack3,
+      BarrelHighBlack4,
+    ],
+    BarrelHighBlue: [
+      BarrelHighBlue1,
+      BarrelHighBlue2,
+      BarrelHighBlue3,
+    ],
+    BarrelHighDarkBlue: [
+      BarrelHighDarkBlue1,
+      BarrelHighDarkBlue2,
+    ],
+},
+  shirtsAndBlouses: {
+    BandCollarBlouseNavyBlue: [
+      BandCollarBlouseNavyBlue1,
+      BandCollarBlouseNavyBlue2,
+      BandCollarBlouseNavyBlue3,
+    ],
+    BasicShacket: [BasicShacket1, BasicShacket2, BasicShacket3],
+    FauxLeatherEmbroideredShirt: [
+      FauxLeatherEmbroideredShirt1,
+      FauxLeatherEmbroideredShirt2,
+      FauxLeatherEmbroideredShirt3,
+    ],
+    FlounceTrimmedBlouse: [
+      FlounceTrimmedBlouse1,
+      FlounceTrimmedBlouse2,
+      FlounceTrimmedBlouse3,
+    ],
+    FlounceTrimmedBlouseCreamFloral: [
+      FlounceTrimmedBlouseCreamFloral1,
+      FlounceTrimmedBlouseCreamFloral2,
+      FlounceTrimmedBlouseCreamFloral3,
+    ],
+    LaceUpDenimShirtDenimBlue: [
+      LaceUpDenimShirtDenimBlue1,
+      LaceUpDenimShirtDenimBlue2,
+      LaceUpDenimShirtDenimBlue3,
+    ],
+    LinenShirtBlack: [LinenShirtBlack1, LinenShirtBlack2, LinenShirtBlack3],
+    LooseFitCottonShirt: [
+      LooseFitCottonShirt1,
+      LooseFitCottonShirt2,
+      LooseFitCottonShirt3,
+    ],
+    LyocellDenimShirt: [
+      LyocellDenimShirt1,
+      LyocellDenimShirt2,
+      LyocellDenimShirt3,
+      LyocellDenimShirt4,
+    ],
+    LyocellDenimShirtDenimBlue: [
+      LyocellDenimShirtDenimBlue1,
+      LyocellDenimShirtDenimBlue2,
+      LyocellDenimShirtDenimBlue3,
+    ],
+    LyocellDenimShirtDenimGrey: [
+      LyocellDenimShirtDenimGrey1,
+      LyocellDenimShirtDenimGrey2,
+      LyocellDenimShirtDenimGrey3,
+    ],
+    NonDenimShacket: [NonDenimShacket1, NonDenimShacket2, NonDenimShacket3],
+    OxfordShirtLightPink: [
+      OxfordShirtLightPink1,
+      OxfordShirtLightPink2,
+      OxfordShirtLightPink3,
+    ],
+    PopoverDenimShirtCream: [
+      PopoverDenimShirtCream1,
+      PopoverDenimShirtCream2,
+      PopoverDenimShirtCream3,
+    ],
+    SequinedBlouseDarkGrey: [
+      SequinedBlouseDarkGrey1,
+      SequinedBlouseDarkGrey2,
+      SequinedBlouseDarkGrey3,
+    ],
+    WomensBlousesShirtsDenimSatinLinen: [
+      WomensBlousesShirtsDenimSatinLinen1,
+      WomensBlousesShirtsDenimSatinLinen2,
+      WomensBlousesShirtsDenimSatinLinen3,
+      WomensBlousesShirtsDenimSatinLinen4,
+    ],
+  },
+  trousers: {
+    CigaretteTrousersBlack: [
+      CigaretteTrousersBlack1,
+      CigaretteTrousersBlack2,
+      CigaretteTrousersBlack3,
+    ],
+    FineKnitTrousersDarkBeige: [
+      FineKnitTrousersDarkBeige1,
+      FineKnitTrousersDarkBeige2,
+      FineKnitTrousersDarkBeige3,
+    ],
+    FlaredRibKnitTrousersBlack: [
+      FlaredRibKnitTrousersBlack1,
+      FlaredRibKnitTrousersBlack2,
+      FlaredRibKnitTrousersBlack3,
+    ],
+    FlaredSportsLeggingsInDryMove: [
+      FlaredSportsLeggingsInDryMove1,
+      FlaredSportsLeggingsInDryMove2,
+      FlaredSportsLeggingsInDryMove3,
+    ],
+    HighWaistedLeggingsBlack: [
+      HighWaistedLeggingsBlack1,
+      HighWaistedLeggingsBlack2,
+      HighWaistedLeggingsBlack3,
+    ],
+    SweatpantsBlack: [SweatpantsBlack1, SweatpantsBlack2, SweatpantsBlack3],
+    TailoredJerseyTrousersNavy: [
+      TailoredJerseyTrousersNavy1,
+      TailoredJerseyTrousersNavy2,
+      TailoredJerseyTrousersNavy3,
+      TailoredJerseyTrousersNavy4,
+    ],
+    WideJoggersPigeonBlue: [
+      WideJoggersPigeonBlue1,
+      WideJoggersPigeonBlue2,
+      WideJoggersPigeonBlue3,
+    ],
+    WideTailoredTrousers: [
+      WideTailoredTrousers1,
+      WideTailoredTrousers2,
+      WideTailoredTrousers3,
+    ],
+    WideTwillTrousersBlack: [
+      WideTwillTrousersBlack1,
+      WideTwillTrousersBlack2,
+      WideTwillTrousersBlack3,
+    ],
+  },
+};
