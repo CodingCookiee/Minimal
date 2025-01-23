bc97060db7259f0ed4e01552d2bce9773d775d2e
CodingCookiee <rzqutubawan@gmail.com>
2025-01-23 18:57:31 +0500

    Update

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 router.get("/search", searchProducts);
 router.post("/", authenticateUser, authenticateAdmin, createProduct);
-router.get("/", authenticateUser, getAllProducts);
+router.get("/", getAllProducts);
 router.get("/:id", getSingleProduct);
 router.get("/:type/:category?", getProductsByTypeAndCategory);
 router.delete("/:id", authenticateUser, authenticateAdmin, deleteProduct);
