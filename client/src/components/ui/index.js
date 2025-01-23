99485a048a3423853059f1a3c8f77e316359ab0d
CodingCookiee <rzqutubawan@gmail.com>
2025-01-23 19:09:47 +0500

    update

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -16,7 +16,7 @@ export * from "./ProductCard";
 export * from "./ProductList";
 export * from "./Dialog";
 export * from "./Loading";
-export * from "./imageDropzone";
+export * from "./ImageDropzone";
 export * from "./LoadingDots";
 export * from "./CheckoutForm";
 export { default as ProductCard } from "./ProductCard";
