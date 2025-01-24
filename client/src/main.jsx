b0326272dd16950b12812326d8e10c2df078bb95
CodePanda-crypto <rzqutubawan@gmail.com>
2025-01-23 23:55:34 -0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,7 +16,20 @@ createRoot(document.getElementById("root")).render(
         <CartProvider>
           <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
             <App />
-            <ToastContainer position="top-right" autoClose={3000} />
+            <ToastContainer
+position="bottom-right"
+autoClose={2000}
+limit={1}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="dark"
+transition={Slide}
+/>
           </GoogleOAuthProvider>
         </CartProvider>
       </OrderProvider>
